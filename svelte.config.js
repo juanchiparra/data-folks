1a7bea99256d798d819baa6f66ad933bad2e3a24
Juanchi Parra <juanchiparra@gmail.com>
2025-03-18 00:19:31 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
-export default {
+const config = {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
-    prerender: {
-      handleHttpError: ({ path, referrer, message }) => {
-        if (path === '/projects') {
-          return;
-        }
-        console.error('Prerendering error:', message);
+      adapter: adapter(),
+      paths: {
+          base: '/data-folks'
       }
-    }
   }
 };
+
+export default config;
