9b5d343f1b8ff1ff34a53d4278a17e927e478f97
Juanchi Parra <juanchiparra@gmail.com>
2026-02-09 13:34:14 -0400

    Updates

--- _app/immutable/entry/start.2ULczJ3m.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/DxC4NJ35.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.DvyO8bPO.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/Db5uB-Mk.js";export{o as load_css,r as start};
