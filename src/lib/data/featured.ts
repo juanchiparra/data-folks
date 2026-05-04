14ca7f91e63e81162b021e8dce3e55b90c433aa8
Juanchi Parra <juanchiparra@gmail.com>
2026-05-04 12:26:04 -0400

    feat(data): update featured folk of the week

--- src/lib/data/featured.ts
+++ src/lib/data/featured.ts
@@ -1,8 +1,8 @@
 import type { FeaturedFolk } from "$lib/types";
 
 export const weeklyFeatured: FeaturedFolk = {
-    folkId: 83,
-    reason: "Nathan is a statistician and the founder of <b>FlowingData</b>, in which he explores how we can use data to understand our world. He is the author of <b>'Visualize This'</b> and <b>'Data Points'</b>, which focus on the practical application of visualization techniques. His approach is essential for anyone looking to bridge the gap between <b>statistical analysis</b> and high-quality <b>interactive information design</b>. His tutorials are an incredible resource for anyone interested in learning about data visualization, and if you have the chance, be sure to take advantage of the membership he offers on <b>FlowingData</b>",
+    folkId: 9,
+    reason: "Amelia is a designer and developer who explores the intersection of data, interfaces, and storytelling. She is the author of Fullstack D3 and Data Visualization, a book that was essential to my learning process for D3. There's also a course version. Her teaching style is highly practical and thoroughly explores the fundamentals. Also, she has tutorials on her website and always shares updates about her work and experiments on social media, so I recommend following her to learn from her",
     reasonMobile:
-        "Nathan is a statistician and founder of <b>FlowingData</b>. Author of <b>'Visualize This'</b> and <b>'Data Points'</b>. His tutorials are an incredible resource for bridging the gap between <b>statistical analysis</b> and <b>interactive information design</b>",
+        "Amelia is a <b>designer and developer</b> who explores the intersection of <b>data, interfaces, and storytelling</b>. She is the author of <b>Fullstack D3 and Data Visualization</b>, a book that was essential to my learning process for <b>D3</b>",
 };
