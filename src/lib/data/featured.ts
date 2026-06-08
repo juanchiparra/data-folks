1ebe4ae10fe9b14b57ea2b9f8c3722a4a7df9b8c
Juanchi Parra <juanchiparra@gmail.com>
2026-06-08 11:19:28 -0400

    feat(data): update featured folk of the week

--- src/lib/data/featured.ts
+++ src/lib/data/featured.ts
@@ -1,8 +1,8 @@
 import type { FeaturedFolk } from "$lib/types";
 
 export const weeklyFeatured: FeaturedFolk = {
-    folkId: 8,
-    reason: 'Alvin is a data journalist specializing in <b>interactive storytelling</b>. After leading the visuals and data team at <a href="https://www.theguardian.com/us" target="_blank">The Guardian US</a>, he became a <b>professor</b> at <a href="https://www.newschool.edu/" target="_blank">The New School</a> and a regular contributor to <a href="https://pudding.cool/" target="_blank">The Pudding</a>. I follow his work for its <b>distinct visual language</b> and the way he uses personal and everyday topics to tell stories. For those interested in process, his <b><a href="https://buttondown.com/charthead/archive" target="_blank">"Behind the Scenes"</a></b> resources are essential for understanding how to build comprehensive narratives. I owe it to him that I’ve become a better information designer (one who\'s more curious and detail-oriented) and someone who understands that <b>creativity knows no bounds</b>. It\'s also thanks to him that I discovered <b>p5.js</b>',
+    folkId: 56,
+    reason: "Krisztina is an information designer specializing in <b>interactive data visualizations</b>. She has been recognized in multiple editions of the <b>Information is Beautiful Awards</b>. I discovered her work in 2021 through her incredible visualizations of the <b>Olympics</b>, which completely opened my eyes to the immense interactive potential that <b>D3.js</b> could offer. From sports data to complex global challenges, her portfolio is full of great interactive examples",
     reasonMobile:
-        'Alvin is a data journalist specializing in <b>interactive storytelling</b>. I follow his work for its distinct visual language and the way he uses everyday topics to tell stories. His <b><a href="https://buttondown.com/charthead/archive" target="_blank">"Behind the Scenes"</a></b> resources are essential for understanding how to build comprehensive narratives',
+        "Krisztina is an information designer specializing in <b>interactive data visualizations</b>. I discovered her work in 2021 through her incredible visualizations of the <b>Olympics</b>, which completely opened my eyes to the interactive potential of <b>D3.js</b>",
 };
