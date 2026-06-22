5d98f385006133af97a15e14f422b6c83490d964
Juanchi Parra <juanchiparra@gmail.com>
2026-06-22 08:03:12 -0400

    feat(data): update featured folk of the week

--- src/lib/data/featured.ts
+++ src/lib/data/featured.ts
@@ -1,8 +1,8 @@
 import type { FeaturedFolk } from "$lib/types";
 
 export const weeklyFeatured: FeaturedFolk = {
-    folkId: 99,
-    reason: "Sarah is a freelance <b>accessibility specialist</b> and front-end developer who teaches about <b>inclusive design</b>. They are one of the leading figures in the field of <b>accessible data visualization</b> and <b>ethical technology</b>. I discovered them a few years ago through their articles on <b>accessibility in dataviz</b>, and they were a real eye-opener for me. Although I still have a lot to improve on, I try to apply their advice in most of my projects. I always end up asking myself, 'What would Sarah say about this?' Their <b>guides and resources</b> are incredibly valuable, especially since so few people address these topics with such depth. There's a lot to learn on their website",
+    folkId: 83,
+    reason: "Nathan is a statistician and the founder of <b>FlowingData</b>, in which he explores how we can use data to understand our world. He is the author of <b>'Visualize This'</b> and <b>'Data Points'</b>, which focus on the practical application of visualization techniques. His approach is essential for anyone looking to bridge the gap between <b>statistical analysis</b> and high-quality <b>interactive information design</b>. His tutorials are an incredible resource for anyone interested in learning about data visualization, and if you have the chance, be sure to take advantage of the membership he offers on <b>FlowingData</b>",
     reasonMobile:
-        "Sarah is a freelance <b>accessibility specialist</b> and front-end developer who teaches about <b>inclusive design</b>. I discovered them a few years ago through their articles on <b>accessibility in dataviz</b>, and they were a real eye-opener for me. Their <b>guides and resources</b> are incredibly valuable.",
+        "Nathan is a statistician and founder of <b>FlowingData</b>. Author of <b>'Visualize This'</b> and <b>'Data Points'</b>. His tutorials are an incredible resource for bridging the gap between <b>statistical analysis</b> and <b>interactive information design</b>",
 };
