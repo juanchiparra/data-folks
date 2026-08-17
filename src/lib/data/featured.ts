f16f968a8524e45ed205193d5c7ccba4136d8a4a
Juanchi Parra <juanchiparra@gmail.com>
2026-08-17 12:23:08 -0400

    feat(data): update featured folk of the week

--- src/lib/data/featured.ts
+++ src/lib/data/featured.ts
@@ -1,5 +1,5 @@
 import type { FeaturedFolk } from "$lib/types";
 
 export const weeklyFeatured: FeaturedFolk = {
-    folkId: 99,
+    folkId: 83,
 };
--- src/lib/data/folks.ts
+++ src/lib/data/folks.ts
@@ -702,6 +702,9 @@ export const folks: Folk[] = [
             page: "https://flowingdata.com/",
             type: "mastermind",
             image: images["/src/lib/images/nathan-yau.webp"].default,
+            reason: "Nathan is a statistician and the founder of <b>FlowingData</b>, in which he explores how we can use data to understand our world. He is the author of <b>'Visualize This'</b> and <b>'Data Points'</b>, which focus on the practical application of visualization techniques. His approach is essential for anyone looking to bridge the gap between <b>statistical analysis</b> and high-quality <b>interactive information design</b>. His tutorials are an incredible resource for anyone interested in learning about data visualization, and if you have the chance, be sure to take advantage of the membership he offers on <b>FlowingData</b>",
+            reasonMobile:
+                "Nathan is a statistician and founder of <b>FlowingData</b>. Author of <b>'Visualize This'</b> and <b>'Data Points'</b>. His tutorials are an incredible resource for bridging the gap between <b>statistical analysis</b> and <b>interactive information design</b>",
         },
     },
     {
