e9c0d59e9b827b099c3758e15874378eec5ba865
Juanchi Parra <juanchiparra@gmail.com>
2026-09-07 07:45:35 -0400

    feat(data): update featured folk of the week

--- src/lib/data/folks.ts
+++ src/lib/data/folks.ts
@@ -420,6 +420,10 @@ export const folks: Folk[] = [
       page: "https://www.linkedin.com/in/juanpablobravocl/",
       type: "designer",
       image: images["/src/lib/images/juan-pablo-bravo.webp"].default,
+      reason:
+        "Juan Pablo is an infographic and professor specializing in <b>news infographics and information design</b>. With extensive experience at <a href='https://elmercurio.com/' target='_blank'><b>El Mercurio</b></a>, he balances his work in the newsroom with teaching multimedia storytelling at several universities in Chile. He was one of the first infographic designers I discovered, and it's inspiring to see such remarkable work emerging from <b>Latin America</b>, especially since he is also training future professionals. I initially came across his work through his visual analysis of <a href='https://infografias.elmercurio.com/20180623-VA-vincentvangogh/' target='_blank'><b>Vincent van Gogh</b></a> and <a href='https://infografias.elmercurio.com/20181201-VA-edwardhopper/' target='_blank'><b>Edward Hopper</b></a>, which are two excellent examples that showcase his talent",
+      reasonMobile:
+        "Juan Pablo is an infographic and professor specializing in <b>news infographics</b>. With extensive experience at <a href='https://elmercurio.com/' target='_blank'><b>El Mercurio</b></a>, he balances his newsroom work with teaching. I initially discovered his amazing work through his visual analysis of <a href='https://infografias.elmercurio.com/20180623-VA-vincentvangogh/' target='_blank'><b>Vincent van Gogh</b></a> and <a href='https://infografias.elmercurio.com/20181201-VA-edwardhopper/' target='_blank'><b>Edward Hopper</b></a>",
     },
   },
   {
