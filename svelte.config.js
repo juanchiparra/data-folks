7b2b2baa236154fa2c2f60b3efc8685f54bf3774
Juanchi Parra <juanchiparra@gmail.com>
2026-09-24 14:16:20 -0400

    chore(tooling): align gitignore, svelte config and versions (#93)
    * chore(tooling): ignore husky internals and coverage
    * chore(core): add vite preprocess and runes

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,13 @@
 import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: vitePreprocess(),
+
+  compilerOptions: {
+    runes: true,
+  },
   kit: {
     adapter: adapter({
       pages: "build",
